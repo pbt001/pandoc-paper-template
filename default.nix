b0add3d7db626f43101031ad8166ed4fae7367a4
Matthias Beyer <mail@beyermatthias.de>
2015-12-24 16:59:33 +0100

    Provide tex environment with texlive

--- default.nix
+++ default.nix
@@ -4,6 +4,28 @@ let
   env = with pkgs.haskellPackages; [
     pandoc
     pandoc-citeproc
+
+    (pkgs.texlive.combine {
+      inherit (pkgs.texlive)
+        scheme-small
+        algorithms
+        cm-super
+        collection-basic
+        collection-bibtexextra
+        collection-fontsextra
+        collection-fontutils
+        collection-langenglish
+        collection-langgerman
+        collection-latex
+        collection-latexextra
+        collection-latexrecommended
+        collection-mathextra
+        collection-metapost
+        collection-pictures
+        collection-plainextra
+        collection-science
+      ;
+    })
   ];
 in
 
