bcb59e06b7c4ef15b37383bfebd766d7ef156ab2
Matthias Beyer <mail@beyermatthias.de>
2016-01-16 22:48:40 +0100

    Add msc-filter

--- scripts/piping.sh
+++ scripts/piping.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-LIST_OF_FILTERS=$(dirname ${BASH_SOURCE[0]})/graphviz-filter.py
+LIST_OF_FILTERS=$(dirname ${BASH_SOURCE[0]})/graphviz-filter.py \
+                $(dirname ${BASH_SOURCE[0]})/msc-filter.py
 
 for filter in $LIST_OF_FILTERS
 do
